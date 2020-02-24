e11a967c92af05a8863fae539b3c9f2403213783
Stephen Eisen <stephen.eisen27@gmail.com>
2020-02-24 15:24:48 -0500

    Add remove functionallity implemented

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -6,13 +6,21 @@ import { Component } from '@angular/core';
 })
 export class TaskListComponent {
     taskData = [];
+    showForm = false;
 
     constructor() {
-        this.taskData.push({ name: 'Task#1', description: 'Description#1' });
-        this.taskData.push({ name: 'Task#2', description: 'Description#2' });
-        this.taskData.push({ name: 'Task#3', description: 'Description#3' });
-        this.taskData.push({ name: 'Task#4', description: 'Description#4' });
+    }
 
-        console.log(this.taskData);
+    showTaskForm() {
+        this.showForm = true;
+    }
+
+    addTask(name:string, description:string) {
+        this.taskData.unshift({name: name, description: description});
+        this.showForm = false;
+    }
+
+    deleteTask(i:number) {
+        this.taskData.splice(i, 1);
     }
 }
